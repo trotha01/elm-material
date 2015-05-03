00e52cb3eff1b4e5d1b2a8b39cae165f66bd24b6
Trevor Rothaus <trevor.rothaus@sendgrid.com>
2015-05-03 14:30:00 -0700

    modularized a bit more

--- navigation-drawer.elm
+++ navigation-drawer.elm
@@ -4,26 +4,49 @@ import Graphics.Element exposing (Element)
 import Graphics.Element as G -- exposing (Element, centered, container, color, flow, right, spacer)
 import Graphics.Input exposing (clickable)
 import Text exposing (fromString)
-import Debug
+import Maybe exposing (Maybe)
+import List
 
 main : Signal Element
-main = click.signal
+main = action.signal
+  |> Signal.foldp update (MainView helloWorld)
   |> Signal.map2 view Window.dimensions
 
 -- MODEL
 type State
-    = MainView
-    | NavBar
+    = MainView Page 
+    | NavBar Page 
+
+type Action
+    = OpenNavDrawer
+    | CloseNavDrawer (Maybe Page) -- Might change content
+
+
+type alias Page = { content:Element }
+
+helloWorld : Page
+helloWorld= { content = G.centered (fromString "Hello World") }
+
+stuff : Page
+stuff = { content = G.centered (fromString "Stuff") }
+
+pages : List Page
+pages = [ helloWorld, stuff]
+
+
 
 -- INPUTS
-click : Signal.Mailbox State
-click =
-  Signal.mailbox MainView
+action : Signal.Mailbox Action
+action =
+  Signal.mailbox (OpenNavDrawer)
 
 -- DISPLAY
 
 title : String -> Element
 title string = G.centered (fromString string)
+
+welcomePage : Element
+welcomePage = G.centered (fromString "Hello World")
 
 hamburger : Element
 hamburger = G.image 60 60 "hamburger.svg"
@@ -31,40 +54,65 @@ hamburger = G.image 60 60 "hamburger.svg"
 -- clickable : Signal.Message -> Element -> Element
 -- button : Signal.Message -> String -> Element
 
+body : (Int, Int) -> Element -> Element
+body (w, h) content =
+  G.container w h G.middle content
+
 -- toolbar takes in a width (of the screen) and a title string
-toolbar : Int -> String -> Element
-toolbar width string =
-  G.container width 180 G.midLeft
+toolbar : Int -> Int -> String -> Element
+toolbar width height string =
+  G.container width height G.midLeft
   (G.flow G.right
     [ G.spacer 60 1
     , hamburger
-        |> clickable (Signal.message click.address NavBar)
+        |> clickable (Signal.message action.address OpenNavDrawer)
     , G.spacer 180 1
     , title string
     ]
   )
   |> G.color green
 
+drawerOption : Page -> Element
+drawerOption page =
+      G.container 340 100 G.middle page.content
+          |> clickable (Signal.message action.address (CloseNavDrawer (Just page)))
+
 -- NavigationDrawer comes in from the left
 navigationDrawer width height =
   G.flow G.right
   [ G.flow G.down
-      [ G.container 340 100 G.middle (title "Hello World")
-      , G.container 340 100 G.middle (title "More Stuff!")
-      ]
+    (List.map drawerOption pages)
     |> G.color white
   , G.spacer (width) height
       |> G.color black
       |> G.opacity (0.5)
-      |> clickable (Signal.message click.address MainView)
+      |> clickable (Signal.message action.address (CloseNavDrawer Nothing))
   ]
 
 view : (Int, Int) -> State -> Element
 view (w, h) state =
     case state of
-        MainView -> toolbar w "Title"
-        NavBar -> G.layers
-          [ toolbar w "Title"
+        MainView page -> G.flow G.down 
+          [ toolbar w 180 "Title"
+          , body (w, (h-180)) page.content
+          ]
+        NavBar page -> G.layers
+          [ G.flow G.down
+              [ toolbar w 180 "Title"
+              , body (w, (h-180)) page.content
+              ]
           , navigationDrawer w h
           ]
 
+-- UPDATE
+
+update : Action -> State -> State
+update action state =
+  let page =
+    case state of
+      MainView p -> p
+      NavBar p -> p
+  in case action of
+    OpenNavDrawer -> NavBar page
+    CloseNavDrawer (Just newPage) -> MainView newPage
+    CloseNavDrawer (Nothing) -> MainView page
